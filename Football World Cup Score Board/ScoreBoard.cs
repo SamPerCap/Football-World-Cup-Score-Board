c4094ebd55108de0a6cf92b4a7b3812a88398e06
SamPerCap <smperez190@gmail.com>
2024-07-29 19:17:18 +0200

    StartGameTests and code

--- Football World Cup Score Board/ScoreBoard.cs
+++ Football World Cup Score Board/ScoreBoard.cs
@@ -1,14 +1,37 @@
 ﻿using ScoreBoardLibrary.Interfaces;
+using ScoreBoardLibrary.Models;
 
 namespace ScoreBoardLibrary
 {
     public class ScoreBoard : IScoreBoard
     {
+        private readonly List<Game> _onGoingGames;
+        private readonly List<Game> _finishedGames;
+        private readonly HashSet<string> _teamsPlaying;
+
+        public ScoreBoard()
+        {
+            _onGoingGames = [];
+            _finishedGames = [];
+            _teamsPlaying = [];
+        }
+
         public void StartGame(string homeTeam, string awayTeam)
         {
-            if (string.IsNullOrEmpty(homeTeam) || string.IsNullOrEmpty(awayTeam)) {
+            if (string.IsNullOrEmpty(homeTeam) || string.IsNullOrEmpty(awayTeam))
+            {
                 throw new ArgumentException("Team names have not been provided");
             }
+
+            if (_teamsPlaying.Contains(homeTeam) || _teamsPlaying.Contains(awayTeam))
+            {
+                throw new InvalidOperationException("One team is already playing a game");
+            }
+
+            var game = new Game(homeTeam, awayTeam);
+            _onGoingGames.Add(game);
+            _teamsPlaying.Add(homeTeam);
+            _teamsPlaying.Add(awayTeam);
         }
 
         public void FinishGame()
@@ -24,6 +47,16 @@ namespace ScoreBoardLibrary
         public void UpdateGame()
         {
             throw new NotImplementedException();
+        }
+
+        public List<Game> GetOnGoingGames()
+        {
+            return _onGoingGames;
+        }
+
+        public List<Game> GetFinishedGames()
+        {
+            return _finishedGames;
         }
     }
 }
--- ScoreBoard.Tests/StartGameTests.cs
+++ ScoreBoard.Tests/StartGameTests.cs
@@ -1,3 +1,5 @@
+using ScoreBoardLibrary.Models;
+
 namespace ScoreBoardLibrary.Tests
 {
     public class StartGameTests
@@ -16,8 +18,36 @@ namespace ScoreBoardLibrary.Tests
         [InlineData(null, null)]
         public void ShouldThrowExceptionIfNoTeams(string homeTeam, string awayTeam)
         {
-
             Assert.Throws<ArgumentException>(() => _scoreBoard.StartGame(homeTeam, awayTeam));
+        }
+
+        [Theory]
+        [InlineData("Home Team", "Away Team")]
+        public void ShouldInitializeGameWithValidTeamsAndScore(string homeTeam, string awayTeam)
+        {
+            // Act
+            _scoreBoard.StartGame(homeTeam, awayTeam);
+            List<Game> ongoingGames = _scoreBoard.GetOnGoingGames();
+            Game game = ongoingGames[0];
+
+            // Assert
+            Assert.Equal(homeTeam, game.HomeTeam.Name);
+            Assert.Equal(awayTeam, game.AwayTeam.Name);
+            Assert.Equal(0, game.HomeTeam.Score);
+            Assert.Equal(0, game.AwayTeam.Score);
+            Assert.Equal(0, game.TotalScore);
+        }
+
+        [Theory]
+        [InlineData("Home Team", "Away Team")]
+        public void ShouldPreventDuplicated(string homeTeam, string awayTeam)
+        {
+            // Act
+            _scoreBoard.StartGame(homeTeam, awayTeam);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() => _scoreBoard.StartGame(homeTeam, awayTeam));
+            Assert.Single(_scoreBoard.GetOnGoingGames());
         }
     }
 }
